c79fd2c81c6838228c5097fb3972ca5223b08708
noelsilis <noelsilis99@gmail.com>
2021-03-31 01:18:38 -0600

    peisc rediseño

--- head.php
+++ head.php
@@ -28,6 +28,7 @@ session_start();
     <link rel="stylesheet" type="text/css" href="css/investigacion.css?1.0.0" />
     <link rel="stylesheet" type="text/css" href="css/login.css?1.0.0" />
     <link rel="stylesheet" href="css/index.css" />
+    <link rel="stylesheet" href="css/peisc.css" />
     <link rel="stylesheet" type="text/css" href="css/estilos.css" />
     <link rel="stylesheet" type="text/css" href="css/especialidad.css" />
     <link rel="stylesheet" type="text/css" href="css/normalize.css" />
